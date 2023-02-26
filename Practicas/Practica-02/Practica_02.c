ba32a4a29b294aa5bf05028ef1578af76e9e9ed3
Dafne Bonilla <daphnebonilla09@gmail.com>
2023-02-25 22:47:57 -0600

    media armonica funcional

--- Practicas/Practica-02/Practica_02.c
+++ Practicas/Practica-02/Practica_02.c
@@ -2,32 +2,38 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-
 float media_aritmetica(float datos[], int n) {  
     float suma = 0;
-    for (int i = 1; i < n; i++) {
+    for (int i = 2; i < n; i++) {
         suma += datos[i];
     }
     float total;
-    total = (float)n-1;
+    total = (float)n-2;
     float resultado = suma/total;
     return resultado;
 }
 
-
-/*
 float media_armonica(float datos[], int n) {
-    float resultado = 0;
-
+    float suma = 0;
+    for (int i = 2; i < n; i++) {
+        suma += 1/datos[i];
+    }
+    float total;
+    total = (float)n-2;
+    float resultado = total/suma;
     return resultado;
 }
 
 float media_geometrica(float datos[], int n) {
-    float resultado = 0;
-
-    return resultado;
+    float multiplicacion = 1;
+    for (int i = 2; i < n; i++) {
+        multiplicacion = multiplicacion*datos[i];
+    }
+    float potencia;
+    //total = (float)n-2;
+    //float resultado = suma/total;
+    return multiplicacion;
 }
-*/
 
 int main(int argc, char *argv[]) {
     
@@ -44,6 +50,8 @@ int main(int argc, char *argv[]) {
     return 0;
     */
 
-    printf("%f", media_aritmetica(datos, argc));
+    // printf("%f", media_aritmetica(datos, argc));
+    // printf("%f", media_armonica(datos, argc));
+    printf("%f", media_geometrica(datos, argc));
     
 }
